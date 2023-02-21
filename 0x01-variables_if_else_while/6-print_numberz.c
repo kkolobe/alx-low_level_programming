f7b6464011dd09d593a187e996b9468766107327
Keabetswe Kolobe.com <keakrypt@gmail.com>
2023-02-21 02:16:26 +0200

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	purchar('\n');
+
+	return (0);
+}
