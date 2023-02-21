dee278b69677f19a51427ab4c931f56c10f9202d
Keabetswe Kolobe.com <keakrypt@gmail.com>
2023-02-21 02:21:32 +0200

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet in reverse
+ * Return: Always (Success)
+ *
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+	putchar(low);
+	putchar('\n');
+
+	return(0);
+}
