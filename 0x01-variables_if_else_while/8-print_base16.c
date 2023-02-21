30877fe1d4dd4cb060edbc55dae23f867d594ed3
Keabetswe Kolobe.com <keakrypt@gmail.com>
2023-02-21 02:27:14 +0200

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base 16 in lowercase
+ * Return: Always (Success)
+ *
+ */
+
+int main(void)
+{
+	int d;
+	char low;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+
+	for (low = 'a'; low <= 'f'; low++)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+}
