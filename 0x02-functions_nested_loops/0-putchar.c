a6c195f5ffe5b71d450c636c31ebe7e8bfdd8415
Keabetswe Kolobe.com <keakrypt@gmail.com>
2023-02-21 20:24:34 +0200

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <ustdio.h>
+#include <stdlib.h>
+
+/** 
+ * main - Prints _putchar
+ * Return: On success 1.
+ * On error, -1 is returned, and an errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
