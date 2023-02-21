436b6ceef1187cb51e2dea1b882310fb376d096d
Keabetswe Kolobe.com <keakrypt@gmail.com>
2023-02-21 02:48:39 +0200

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints all possible different combinations of two digits
+ * Return: Always (Success)
+ *
+ */
+
+int main(void)
+{
+	int d, p;
+
+	for (d = '0'; d < '9'; d++)
+	{
+	for (p = d + 1; p <= '9'; p++)
+	{
+	if (p != d)
+	putchar(d);
+	putchar(p);
+	if (d == '8' && p == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+
+}
