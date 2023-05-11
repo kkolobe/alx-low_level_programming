754e7fc3b31fa004221fb3060fc5504b1f1d9e21
kkolobe <keakrypt@gmail.com>
2023-05-11 10:10:31 +0200

    Fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,27 +20,27 @@ int main(int argc, char **argv)
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Err: Can't read file %s\n", argv[1]), exit(98);
 
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Err: Can't write to %s\n", argv[2]), exit(99);
 
 	while ((rd = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		wr = write(fd_to, buffer, rd);
 		if (wr != rd)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+			dprintf(STDERR_FILENO, "Err: Can't write to %s\n", argv[2]), exit(99);
 	}
 
 	if (rd == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Err: Can't read file %s\n", argv[1]), exit(98);
 
 	if (close(fd_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+		dprintf(STDERR_FILENO, "Err: Can't close fd %d\n", fd_from), exit(100);
 
 	if (close(fd_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+		dprintf(STDERR_FILENO, "Err: Can't close fd %d\n", fd_to), exit(100);
 
 	return (0);
 }
